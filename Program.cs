15a07f714befa102b64ac99dd02c51c8bfa22164
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-01-19 10:36:10 -0300

    Remove: Comment

--- Program.cs
+++ Program.cs
@@ -4,23 +4,12 @@ using DependencyInjectionTest.UseCase.Transient;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Injeção de dependência
-builder.Services.AddSingleton<SingletonUseCase>();
-builder.Services.AddTransient<TransientUseCase>();
-builder.Services.AddScoped<ScopedUseCase>();
-builder.Services.AddScoped<UseCaseA>();
-builder.Services.AddScoped<UseCaseB>();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
